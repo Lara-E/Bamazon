e8922507b01f63a93679e53d7b111901c8e7c729
Lara Eller <lareh_eller@yahoo.com>
2019-04-30 16:56:05 -0400

    I wrote the seeds.sql

---
+++ mysql/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES 
+    ("13 inch Softball Glove", "Sports Equipment", 22.99, 27),
+    ("Tennis Ball 3 Pack", "Sports Equipment", 1.99, 56),
+    ("32 inch Aluminum Bat", "Sports Equipment", 37.99, 26),
+    ("Nylon Folding Chair", "Camping Equipment", 13.99, 32),
+    ("Leatherman Multi-Tool", "Tools", 49.99, 25),
+    ("Royal Wulff Dry Fly", "Fishing", 0.57, 239),
+    ("4-Person Dome Tent", "Camping Equipment", 44.99, 21),
+    ("Adult Nylon Life Jacket", "Water Sports", 15.99, 43),
+    ("Inflatable Fishing Pontoon", "Fishing", 259.99, 13);
